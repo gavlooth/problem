3bb322e8962944b12e91a6658c3b24b6d403c830
christos chatzifountas <christos.chatzifountas@yahoo.gr>
2025-01-22 22:19:26 +0200

    :tada:

---
+++ src/problem/problem.clj
@@ -0,0 +1,20 @@
+(ns problem.problem
+  (:require [clojure.java.data.builder :as builder])
+  (:import (dev.langchain4j.model.openai OpenAiChatModel)
+           (dev.langchain4j.model.openai OpenAiChatModel$OpenAiChatModelBuilder)))
+
+(defn make-chat-model []
+  (builder/to-java
+    OpenAiChatModel 
+    OpenAiChatModel$OpenAiChatModelBuilder
+    (OpenAiChatModel/builder)
+    {:baseUrl  "localhost"
+     :apiKey (System/getenv "OPENAI_API_KEY")} {}))
+
+(defn -main
+  "Invoke me with clojure -M -m problem.problem"
+  [& args]
+ (try 
+  (make-chat-model)
+  (catch Throwable t (println t))))
+(-main)
