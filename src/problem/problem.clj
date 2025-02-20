0c71ae42772697b27ed7b413e3f06d8181542dda
christos chatzifountas <christos.chatzifountas@biotz.com>
2025-02-20 11:14:19 +0200

    From java problem

--- src/problem/problem.clj
+++ src/problem/problem.clj
@@ -1,20 +1,44 @@
 (ns problem.problem
-  (:require [clojure.java.data.builder :as builder])
-  (:import (dev.langchain4j.model.openai OpenAiChatModel)
-           (dev.langchain4j.model.openai OpenAiChatModel$OpenAiChatModelBuilder)))
+  (:require
+    [clojure.java.data :as jdata]
+    [clojure.java.data.builder :as builder])
+  (:import
+    (io.qdrant.client ConditionFactory)
+    (io.qdrant.client QdrantClient QdrantGrpcClient)
+    (io.qdrant.client WithPayloadSelectorFactory)
+    (io.qdrant.client.grpc Points$Filter Points$Filter$Builder)
+    (io.qdrant.client.grpc Points$ScrollPoints Points$ScrollPoints$Builder)))
 
-(defn make-chat-model []
-  (builder/to-java
-    OpenAiChatModel 
-    OpenAiChatModel$OpenAiChatModelBuilder
-    (OpenAiChatModel/builder)
-    {:baseUrl  "localhost"
-     :apiKey (System/getenv "OPENAI_API_KEY")} {}))
+(defn qdrant-client
+  ([] (QdrantClient. (.build (QdrantGrpcClient/newBuilder "localhost" 6334  false))))
+  ([chan] (QdrantClient. (.build (QdrantGrpcClient/newBuilder chan))))
+  ([host port] (QdrantClient. (.build (QdrantGrpcClient/newBuilder host port false)))))
+
+
+(defn make-scroll-points  [collection-name k v]
+  (builder/to-java Points$ScrollPoints
+                   Points$ScrollPoints$Builder
+                   (Points$ScrollPoints/newBuilder)
+                   {:collectionName collection-name
+                    :filter (builder/to-java Points$Filter
+                                             Points$Filter$Builder
+                                             (Points$Filter/newBuilder)
+                                             {:addMust (ConditionFactory/matchKeyword (name k) (str v))}
+                                             {})
+                    :limit 50
+                    :withPayload (WithPayloadSelectorFactory/enable true)}
+                   {}))
+
+
 
 (defn -main
   "Invoke me with clojure -M -m problem.problem"
-  [& args]
- (try 
-  (make-chat-model)
-  (catch Throwable t (println t))))
-(-main)
+  [& _args]
+  (try
+    (make-scroll-points "name" :key "value")
+    (catch Throwable t (println t))))
+
+(def scroll-points (make-scroll-points "name" "key" "value"))
+
+#_(jdata/from-java scroll-points)
+
